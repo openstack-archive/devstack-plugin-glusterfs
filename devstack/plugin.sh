92458fa50d94c0376463b312386576142c4ea883
Deepak C Shetty <deepakcs@redhat.com>
2015-05-18 17:18:54 +0530

    plugin.sh: Remove setting of os_privileged_user_* params
    devstack with commit 38bee18a2c440b01bd1c2187cb2a62a841e3b7ae
    added support for configuring os_privileged_user_* params so
    we don't need to do it in our plugin code.
    
    Change-Id: I4827d92acba4f4e960177bc76524d94ce0b58095

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -98,14 +98,6 @@ function configure_glusterfs_cinder {
         sudo gluster --mode=script volume start $vol_name
         sudo gluster --mode=script volume set $vol_name server.allow-insecure on
     done
-    configure_privileged_user
-}
-
-# Configure privileged user to support onine_snapshots
-function configure_privileged_user {
-    iniset $CINDER_CONF DEFAULT os_privileged_user_name nova
-    iniset $CINDER_CONF DEFAULT os_privileged_user_password $SERVICE_PASSWORD
-    iniset $CINDER_CONF DEFAULT os_privileged_user_tenant service
 }
 
 # this modifies the cinder.conf file and create glusterfs_shares_config file.
