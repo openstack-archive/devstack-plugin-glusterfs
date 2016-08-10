01772aa4d4daec0b3c456f8f5df2ae3d309ad316
Csaba Henk <chenk@redhat.com>
2016-08-10 16:14:06 +0200

    clean up dispatches over GLUSTERFS_MANILA_DRIVER_TYPE
    Change-Id: Ib8664c5aa71ff4efb64fd95262295fcf322e5a4e

--- manila/post_test_hook.sh
+++ manila/post_test_hook.sh
@@ -28,7 +28,7 @@ for env_var in ${DEVSTACK_LOCAL_CONFIG// / }; do
     export $env_var;
 done
 
-if [[ "$GLUSTERFS_MANILA_DRIVER_TYPE" == "glusterfs-native" ]]; then
+if [[ "${GLUSTERFS_MANILA_DRIVER_TYPE}" == "glusterfs-native" ]]; then
     local BACKEND_NAME="GLUSTERNATIVE"
     iniset $TEMPEST_CONFIG share enable_protocols glusterfs
     iniset $TEMPEST_CONFIG share storage_protocol glusterfs
@@ -40,11 +40,18 @@ if [[ "$GLUSTERFS_MANILA_DRIVER_TYPE" == "glusterfs-native" ]]; then
     # ro access_level is not supported by the driver.
     iniset $TEMPEST_CONFIG share enable_ro_access_level_for_protocols
 else
-    if [[ "$GLUSTERFS_MANILA_DRIVER_TYPE" == "glusterfs-heketi" ]]; then
-        local BACKEND_NAME="GLUSTERFSHEKETI"
-    else
+    case "$GLUSTERFS_MANILA_DRIVER_TYPE" in
+    "glusterfs|glusterfs-nfs")
         local BACKEND_NAME="GLUSTERFS"
-    fi
+        ;;
+    "glusterfs-heketi|glusterfs-nfs-heketi")
+        local BACKEND_NAME="GLUSTERFSHEKETI"
+        iniset $TEMPEST_CONFIG share capability_snapshot_support True
+        ;;
+    *)
+        echo "no BACKEND_NAME for GLUSTERFS_MANILA_DRIVER_TYPE=${GLUSTERFS_MANILA_DRIVER_TYPE}"
+        ;;
+    esac
     iniset $TEMPEST_CONFIG share enable_protocols nfs
     iniset $TEMPEST_CONFIG share enable_ip_rules_for_protocols nfs
     iniset $TEMPEST_CONFIG share storage_protocol NFS
